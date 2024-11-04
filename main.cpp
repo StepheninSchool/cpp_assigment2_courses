b4753dbbb937956719d84aea3c0d0ffa076ef0ad
StepheninSchool <w0488750@campus.nscc.ca>
2024-11-04 12:54:52 -0400

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include "include/Student.h"
+#include <iostream>
+
+int main() {
+    //write code to interact with student objects
+}
---
+++ src/Student.cpp
@@ -0,0 +1,7 @@
+#include "../include/Student.h"
+
+//implement methods from student class
+Student::Student() : name(""), numCourses(0), courseList(nullptr) {
+    std:
+    cout << "Default constructor called\n";
+}
